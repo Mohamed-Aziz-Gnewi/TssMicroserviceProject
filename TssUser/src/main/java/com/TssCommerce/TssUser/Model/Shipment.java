c746a4c3a52edc2e031d030ddb5e1f2ae7274eed
Mohamed Aziz Gnewi <gnewamedaziz99@gmail.com>
2022-09-29 21:18:01 +0200

    the shipment test 3

--- TssUser/src/main/java/com/TssCommerce/TssUser/Model/Shipment.java
+++ TssUser/src/main/java/com/TssCommerce/TssUser/Model/Shipment.java
@@ -13,6 +13,7 @@ import javax.persistence.*;
 public class Shipment {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String companyName;
     private String address;
--- TssUser/src/main/java/com/TssCommerce/TssUser/Model/User.java
+++ TssUser/src/main/java/com/TssCommerce/TssUser/Model/User.java
@@ -15,6 +15,7 @@ import java.util.List;
 public class User {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String firstName;
     private String lastName;
