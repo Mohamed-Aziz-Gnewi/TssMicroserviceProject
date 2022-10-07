0959389e2f4aa5449ac1f652e9d6d7835e1300e9
Mohamed Aziz Gnewi <gnewamedaziz99@gmail.com>
2022-10-07 22:14:26 +0200

    testing request param

--- TssOrder/src/main/java/com/TssCommerce/TssOrder/Proxy/ProductProxy.java
+++ TssOrder/src/main/java/com/TssCommerce/TssOrder/Proxy/ProductProxy.java
@@ -12,7 +12,7 @@ public interface ProductProxy {
 
     @GetMapping("/tssproduct/getProductDao/{id}")
     public ProductDao getProductDao(@PathVariable("id")Long id);
-    @PostMapping("/tssproduct/getSpecificProductDao")
+    @PostMapping("/getSpecificProductDao")
     public List<ProductDao> getSpecificProductDao(@RequestParam List<Long> idList);
     @PostMapping("/tssproduct/testing")
     public String testing();
