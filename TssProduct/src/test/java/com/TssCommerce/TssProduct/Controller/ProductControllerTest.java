f23bd9b03ca5648653e2d9865d38e2c61327be07
Mohamed Aziz Gnewi <gnewamedaziz99@gmail.com>
2022-10-08 23:56:52 +0200

    Random test commit

--- TssProduct/src/test/java/com/TssCommerce/TssProduct/Controller/ProductControllerTest.java
+++ TssProduct/src/test/java/com/TssCommerce/TssProduct/Controller/ProductControllerTest.java
@@ -131,7 +131,7 @@ class ProductControllerTest {
 
         mockMvc.perform(get("/tssproduct/getProductDao/1"))
                 .andExpect(jsonPath("$.productName",is(productDao.getProductName())))
-                .andExpect(status().isBadRequest());
+                .andExpect(status().isOk());
     }
     @Disabled
     @Test
