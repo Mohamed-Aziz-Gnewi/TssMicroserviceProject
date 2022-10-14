7e2e122dace44ff948dcedf41c276e58ee0c6d0f
Mohamed Aziz Gnewi <gnewamedaziz99@gmail.com>
2022-10-14 21:30:10 +0100

    h2 second try

--- TssProduct/src/test/java/com/TssCommerce/TssProduct/TssProductApplicationTests.java
+++ TssProduct/src/test/java/com/TssCommerce/TssProduct/TssProductApplicationTests.java
@@ -1,10 +1,22 @@
 package com.TssCommerce.TssProduct;
 
+import com.TssCommerce.TssProduct.Controller.ProductController;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
+@AutoConfigureMockMvc
+@ExtendWith( SpringExtension.class)
 class TssProductApplicationTests {
+
+	@Autowired
+	ProductController productController;
 
 	@Test
 	void contextLoads() {
