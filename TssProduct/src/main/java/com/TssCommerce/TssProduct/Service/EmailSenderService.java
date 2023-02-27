34e1146ef51dca7a8ee4cfb29a9b3d57a75ff221
Mohamed Aziz Gnewi <gnewamedaziz99@gmail.com>
2023-02-27 17:11:02 +0100

    Spring Email

--- TssProduct/src/main/java/com/TssCommerce/TssProduct/Service/EmailSenderService.java
+++ TssProduct/src/main/java/com/TssCommerce/TssProduct/Service/EmailSenderService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class EmailSenderService {
 
+    @Autowired
     private JavaMailSender javaMailSender;
 
     public void SendEmail(String toEmail,
